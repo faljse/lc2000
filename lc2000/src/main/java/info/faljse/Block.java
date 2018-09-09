a9b9f9a9c37f7f370c1827daede996f3c1b30d89
Martin Kunz <martin.michael.kunz@gmail.com>
2018-09-09 12:29:36 +0200

    custom verifier

--- lc2000/src/main/java/info/faljse/Block.java
+++ lc2000/src/main/java/info/faljse/Block.java
@@ -7,33 +7,36 @@ import java.security.*;
 public class Block {
 
     public int id;
-    public String nextPubKey;
-    public String nestRSAEncryptedAesKey;
-    public String nextAesEncryptedPrivateKey;
+    public byte[] data;
+    public byte[] signature;
+    public PublicKey nextPubKey;
+    public byte[] nestRSAEncryptedAesKey;
+    public byte[] nextAesEncryptedPrivateKey;
     public final static int AES_KEYSIZE=256;
 
-    public void init(PublicKey nextParticipant) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+    public void init(PublicKey nextParticipant, PrivateKey signKey) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, SignatureException {
 
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
         KeyPair keyPair = keyGen.generateKeyPair();
         PrivateKey privKey = keyPair.getPrivate();
-        PublicKey pubKey = keyPair.getPublic();
+        this.nextPubKey=keyPair.getPublic();
+
 
         KeyGenerator generator = KeyGenerator.getInstance("AES");
         generator.init(AES_KEYSIZE); // The AES key size in number of bits
         SecretKey secKey = generator.generateKey();
         Cipher aesCipher = Cipher.getInstance("AES");
         aesCipher.init(Cipher.ENCRYPT_MODE, secKey);
-        byte[] aesEncryptedPrivateKey = aesCipher.doFinal(privKey.getEncoded());
-
+        this.nextAesEncryptedPrivateKey = aesCipher.doFinal(privKey.getEncoded());
 
         Cipher rsaCipher = Cipher.getInstance("RSA");
         rsaCipher.init(Cipher.ENCRYPT_MODE, nextParticipant);
-        byte[] encryptedKey = rsaCipher.doFinal(secKey.getEncoded());
+        this.nestRSAEncryptedAesKey = rsaCipher.doFinal(secKey.getEncoded());
 
-        this.nextAesEncryptedPrivateKey=aesEncryptedPrivateKey.toString();
-        this.nestRSAEncryptedAesKey=encryptedKey.toString();
-        this.nextPubKey=pubKey.toString();
+        Signature privateSignature = Signature.getInstance("SHA256withRSA");
+        privateSignature.initSign(signKey);
+        privateSignature.update(data); //TODO: sign whole block
+        this.signature = privateSignature.sign();
     }
 
 }
---
+++ lc2000/src/main/java/info/faljse/Chain.java
@@ -0,0 +1,37 @@
+package info.faljse;
+
+import java.security.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Chain {
+
+    public List<Block> blocks=new ArrayList<>();
+
+    public void add(Block b) {
+        if(blocks.isEmpty()) {
+            blocks.add(b);
+            return;
+        }
+        Block last=blocks.get(blocks.size()-1);
+        if(b.id!=last.id+1){
+            throw new RuntimeException("wrong block id");
+        }
+        try {
+            if (verify(b.data, b.signature, last.nextPubKey)) {
+                return;
+            }
+            else throw new RuntimeException("Signature doesnt match");
+
+        } catch (SignatureException | InvalidKeyException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean verify(byte[] data, byte[] signature, PublicKey publicKey) throws SignatureException, InvalidKeyException, NoSuchAlgorithmException {
+        Signature publicSignature = Signature.getInstance("SHA256withRSA");
+        publicSignature.initVerify(publicKey);
+        publicSignature.update(data);
+        return publicSignature.verify(signature);
+    }
+}
