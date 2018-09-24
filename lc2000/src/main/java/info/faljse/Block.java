c4fdec7195b6b406d5c2af3c461a730166fa836c
Martin Kunz <martin.michael.kunz@gmail.com>
2018-09-24 19:20:51 +0200

    custom verifier

--- lc2000/src/main/java/info/faljse/Block.java
+++ lc2000/src/main/java/info/faljse/Block.java
@@ -1,7 +1,15 @@
 package info.faljse;
 
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+import javafx.beans.binding.IntegerBinding;
+
 import javax.crypto.*;
 import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 
 public class Block {
@@ -10,7 +18,7 @@ public class Block {
     public byte[] data;
     public byte[] signature;
     public PublicKey nextPubKey;
-    public byte[] nestRSAEncryptedAesKey;
+    public byte[] nextRSAEncryptedAesKey;
     public byte[] nextAesEncryptedPrivateKey;
     public final static int AES_KEYSIZE=256;
 
@@ -31,12 +39,36 @@ public class Block {
 
         Cipher rsaCipher = Cipher.getInstance("RSA");
         rsaCipher.init(Cipher.ENCRYPT_MODE, nextParticipant);
-        this.nestRSAEncryptedAesKey = rsaCipher.doFinal(secKey.getEncoded());
+        this.nextRSAEncryptedAesKey = rsaCipher.doFinal(secKey.getEncoded());
 
         Signature privateSignature = Signature.getInstance("SHA256withRSA");
         privateSignature.initSign(signKey);
         privateSignature.update(data); //TODO: sign whole block
         this.signature = privateSignature.sign();
+    }
+
+    public String toJSON() {
+        Base64.Encoder encoder = Base64.getEncoder().withoutPadding();
+        JsonObject jo=new JsonObject();
+        jo.add("id", id);
+        jo.add("data",encoder.encodeToString(data));
+        jo.add("signature", encoder.encodeToString(signature));
+        jo.add("nextPubKey",encoder.encodeToString(nextPubKey.getEncoded()));
+        jo.add("nextRSAEncryptedAesKey", encoder.encodeToString(nextRSAEncryptedAesKey));
+        jo.add("nextAesEncryptedPrivateKey", encoder.encodeToString(nextAesEncryptedPrivateKey));
+        return jo.toString();
+    }
+
+    public void fromJSON(String json) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        Base64.Decoder decoder = Base64.getDecoder();
+        JsonObject jo = Json.parse(json).asObject();
+        id=jo.getInt("id",0);
+        data=decoder.decode(jo.getString("data",""));
+        signature=decoder.decode(jo.getString("signature",""));
+        X509EncodedKeySpec spec =
+                new X509EncodedKeySpec(decoder.decode(jo.getString("nextPubKey","")));
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        this.nextPubKey=kf.generatePublic(spec);
     }
 
 }
---
+++ lc2000/src/main/java/info/faljse/Settings.java
@@ -0,0 +1,8 @@
+package info.faljse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Settings {
+    public List<String> hosts=new ArrayList();
+}
