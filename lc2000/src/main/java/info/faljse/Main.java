741b8169f5c52b870d6ec29ed083bb30d0a8ed3c
Martin Kunz <martin.michael.kunz@gmail.com>
2019-04-11 11:50:08 +0200

    switch to spark

--- lc2000/src/main/java/info/faljse/Main.java
+++ lc2000/src/main/java/info/faljse/Main.java
@@ -1,44 +1,23 @@
 package info.faljse;
 
-
-import org.nanohttpd.util.ServerRunner;
-
 import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.security.*;
-import java.security.interfaces.ECPrivateKey;
-import java.security.interfaces.ECPublicKey;
 
 public class Main {
-
     public static Chain chain=new Chain();
-
-
     public static void main(String args[]) {
-        // System.setProperty("java.net.preferIPv4Stack" , "true");
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                ServerRunner.run(WebIf.class);
-            }
-        }).start();
         try {
-            new Main().crypto();
-        } catch ( Exception e) {
+            new WebIf();
+            crypto();
+        } catch (IOException | NoSuchAlgorithmException | IllegalBlockSizeException | InvalidKeyException | BadPaddingException | SignatureException | NoSuchPaddingException e) {
             e.printStackTrace();
         }
-
-        // new Main().bla();
     }
 
-    private void crypto() throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, SignatureException, NoSuchPaddingException {
+    private static void crypto() throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, SignatureException, NoSuchPaddingException {
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
         Block b1=new Block();
         KeyPair pair1 = keyGen.generateKeyPair();
@@ -48,10 +27,5 @@ public class Main {
         KeyPair pair2 = keyGen.generateKeyPair();
         b2.init(pair2.getPublic(), pair2.getPrivate(), "zweiter".getBytes());
         chain.blocks.add(b2);
-
-
     }
-
-
-
 }
--- lc2000/src/main/java/info/faljse/WebIf.java
+++ lc2000/src/main/java/info/faljse/WebIf.java
@@ -2,105 +2,31 @@ package info.faljse;
 import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonArray;
 import com.eclipsesource.json.JsonObject;
-import org.nanohttpd.protocols.http.IHTTPSession;
-import org.nanohttpd.protocols.http.response.IStatus;
-import org.nanohttpd.protocols.http.response.Response;
-import org.nanohttpd.protocols.http.response.Status;
-import org.nanohttpd.router.RouterNanoHTTPD;
-
 import java.io.*;
-import java.util.Map;
+import static spark.Spark.*;
 
-
-public class WebIf extends RouterNanoHTTPD {
+public class WebIf {
     private static final int PORT = 9090;
 
     public WebIf() throws IOException {
-        super(PORT);
+        port(PORT);
         addMappings();
+        awaitInitialization();
         System.out.println("\nRunning! Point your browers to http://localhost:" + PORT + "/ \n");
     }
 
-    @Override
     public void addMappings() {
-        super.addMappings();
-
-
-        addRoute("/blocks", BlockHandler.class);
-        addRoute("/user/help", BlockHandler.class);
-        addRoute("/user/:id", BlockHandler.class);
-        addRoute("/general/:param1/:param2", GeneralHandler.class);
-        addRoute("/photos/:customer_id/:photo_id", null);
-        addRoute("/test", String.class);
-        addRoute("/interface", UriResponder.class); // this will cause an error
-        // when called
-        addRoute("/toBeDeleted", String.class);
-        removeRoute("/toBeDeleted");
-        addRoute("/stream", StreamUrl.class);
-
-        addRoute("/static(.)+", StaticPageTestHandler.class, new File("webroot/").getAbsoluteFile());
-        addRoute("/", StaticPageTestHandler.class, new File("webroot/index.html").getAbsoluteFile());
-    }
-
-    static public class StreamUrl extends DefaultStreamHandler {
-
-        @Override
-        public String getMimeType() {
-            return "text/plain";
-        }
-
-        @Override
-        public IStatus getStatus() {
-            return Status.OK;
-        }
-
-        @Override
-        public InputStream getData() {
-            return new ByteArrayInputStream("a stream of data ;-)".getBytes());
-        }
-
-    }
-
-    public static class StaticPageTestHandler extends StaticPageHandler {
-
-
-        @Override
-        protected BufferedInputStream fileToInputStream(File fileOrdirectory) throws IOException {
-            if ("exception.html".equals(fileOrdirectory.getName())) {
-                throw new IOException("trigger something wrong");
-            }
-            return super.fileToInputStream(fileOrdirectory);
-        }
-    }
-
-    public static class BlockHandler extends DefaultHandler {
-
-        @Override
-        public String getMimeType() {
-            return MIME_PLAINTEXT;
-        }
-
-        @Override
-        public String getText() {
-            return "not implemented";
-        }
-
-        @Override
-        public IStatus getStatus() {
-            return Status.OK;
-        }
-
-        @Override
-        public Response get(UriResource uriResource, Map<String, String> urlParams, IHTTPSession session) {
+        get("/", (request, response) -> {return "JO!";});
+        post("/addBlock", (req, res) -> {
+            var json=Json.parse(req.body()).asObject();
 
             JsonArray blocks = Json.array();
-            for(Block b: Main.chain.blocks) {
+            for (Block b : Main.chain.blocks) {
                 JsonObject o = new JsonObject();
                 o.add("data", b.data.toString());
                 blocks.add(o);
             }
-
-            return Response.newFixedLengthResponse(blocks.toString());
-        }
+            return "";
+        });
     }
 }
